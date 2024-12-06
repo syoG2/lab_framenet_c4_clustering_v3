a60638c86ce857f0b9898cc9d41a7c8c50d3d54d
syoG2 <syo53594f@gmail.com>
2024-12-06 15:11:53 +0900

    単語レベルを文字レベルに直す

--- src/make_datasets/preprocess_framenet.py
+++ src/make_datasets/preprocess_framenet.py
@@ -26,18 +26,19 @@ class FramenetData(BaseData):
     # 前処理後のframenet
     # source: str  # データの取得元(e.g. framenet)
     id_data: FramenetId  # 元データの参照に必要な情報を格納
-    # text: str  # 前処理前のtext
-    # target_word: str  # 注目する語(基本形)
-    # target_word_idx: int  # 注目する語の位置(単語レベル)
+    # target_word: str  # 注目する語(text内に現れる形)
+    # target_word_idx: list[int]  # 注目する語の位置(文字レベル)[開始位置,終了位置]
     # preprocessed_text: str  # 前処理後のtext
-    # preprocessed_target_widx: list[int]  # 前処理後のLUの位置(単語レベル)[開始位置,終了位置,主となる語の位置(構文木)]
+    # preprocessed_lu_idx: list[list[int]]  # 前処理後のLUの位置(文字レベル)[開始位置,終了位置]
+    # text: str  # 前処理前のtext
+    # uuid: UUID = Field(default_factory=lambda: uuid4())  # 分割後のデータに対して一意に与える
     frame_name: str
     frame_id: int
     lu_name: str
     lu_id: int
-    fe_widx: list[
+    fe_idx: list[
         list[list[int | str]] | dict[str, str]
-    ]  # 前処理後のfeの位置(単語レベル)[[開始位置,終了位置,主となる語の位置(構文木)],{省略されているfe名,省略の種類}]
+    ]  # 前処理後のfeの位置(文字レベル)[[開始位置,終了位置],{省略されているfe名,省略の種類}]
 
 
 class FramenetWordList(WordList):
@@ -45,19 +46,17 @@ class FramenetWordList(WordList):
     # words: list[WordInfo]
 
 
-def get_target_word_idx(text: str, preprocessed_text: str, target: list[list[int]]) -> list[list[int]]:
-    # 文字レベルのindexを単語レベルに変更
-    # FrameNetのtargetは初めのindexの文字は含み、終わりのindexの文字は含まないが、単語レベルのindexに変更する時は初めのindexも終わりのindexも含める
-    char_to_word, _ = get_alignments(list(text), preprocessed_text.split())
-    return [[char_to_word[t[0]][0], char_to_word[t[1] - 1][-1]] for t in target]
+def get_lu_idx(text: str, preprocessed_text: str, target: list[list[int]]) -> list[list[int]]:
+    # 前処理前後でtargetの位置を揃える(文字レベル)
+    text_to_preprocessed_text, _ = get_alignments(list(text), list(preprocessed_text))
+    return [[text_to_preprocessed_text[t[0]][0], text_to_preprocessed_text[t[1] - 1][-1] + 1] for t in target]
 
 
-def get_fe_word_idx(text: str, preprocessed_text: str, fe: list[list[list[int | str]] | dict]) -> list[list[int]]:
-    # 文字レベルのindexを単語レベルに変更
-    # FrameNetのtargetは初めのindexの文字は含み、終わりのindexの文字は含まないが、単語レベルのindexに変更する時は初めのindexも終わりのindexも含める
-    char_to_word, _ = get_alignments(list(text), preprocessed_text.split())
+def get_fe_idx(text: str, preprocessed_text: str, fe: list[list[list[int | str]] | dict]) -> list[list[int]]:
+    # 前処理前後でfeの位置を揃える(文字レベル)
+    text_to_preprocessed_text, preprocessed_text_to_text = get_alignments(list(text), list(preprocessed_text))
     ret: list[list[list[int | str]] | dict] = [[], {}]
-    ret[0] = [[char_to_word[f[0]][0], char_to_word[f[1] - 1][-1], f[2]] for f in fe[0]]
+    ret[0] = [[text_to_preprocessed_text[f[0]][0], text_to_preprocessed_text[f[1] - 1][-1] + 1, f[2]] for f in fe[0]]
     ret[1] = fe[1]
     return ret
 
@@ -117,41 +116,46 @@ def main():
     )
 
     df = pd.read_json(args.input_file, lines=True)
+
     df = df[df["lu_name"].str.contains(r"\.v")]  # 動詞を抽出
     df = df[
         df["lu_name"].apply(lambda lu_name: len(re.sub(r"(\.v)|(\[.*?\])|(\(.*?\))", "", lu_name).split()))
         == df["target"].apply(lambda target: len(target))
     ]  # LUの単語数とtargetの単語数が一致するものを抽出(アノテーションミスと見られるものを省く)
 
+    tqdm.pandas(desc="doc")
+    df["doc"] = df["text"].progress_apply(lambda x: nlp(normalize("NFKC", x)))  # Unicode正規化
+
     tqdm.pandas(desc="preprocessed_text")
-    df["preprocessed_text"] = df["text"].progress_apply(lambda x: normalize("NFKC", x))  # Unicode正規化
+    df["preprocessed_text"] = df["doc"].progress_apply(
+        lambda x: " ".join([word.text for sent in x.sentences for word in sent.words])
+        + " "  # 末尾に空白を追加しないと[,,,"a"," "]と[,,,," ","a"]のアラインメントをとる時におかしくなる
+    )
 
-    tqdm.pandas(desc="preprocessed_target_widx")
-    df["preprocessed_target_widx"] = df.progress_apply(
-        lambda row: get_target_word_idx(row["text"], row["preprocessed_text"], row["target"]), axis=1
-    )  # targetの位置を単語単位に変換
+    tqdm.pandas(desc="preprocessed_lu_idx")
+    df["preprocessed_lu_idx"] = df.progress_apply(
+        lambda row: get_lu_idx(row["text"], row["preprocessed_text"], row["target"]), axis=1
+    )
 
-    tqdm.pandas(desc="fe_widx")
-    df["fe_widx"] = df.progress_apply(
-        lambda row: get_fe_word_idx(row["text"], row["preprocessed_text"], row["fe"]), axis=1
+    tqdm.pandas(desc="fe_idx")
+    df["fe_idx"] = df.progress_apply(
+        lambda row: get_fe_idx(row["text"], row["preprocessed_text"], row["fe"]), axis=1
     )  # feの位置を単語単位に変換
 
     tqdm.pandas(desc="target_word_idx")
     df["target_word_idx"] = df.progress_apply(
-        lambda row: row["preprocessed_target_widx"][0][0]
+        lambda row: row["preprocessed_lu_idx"][0]
         if " " not in row["lu_name"]
-        else row["preprocessed_target_widx"][get_verb_idx(nlp(re.sub(r"(\.v)|(\[.*?\])|(\(.*?\))", "", row["lu_name"]).strip()))][
-            0
-        ],
+        else row["preprocessed_lu_idx"][get_verb_idx(nlp(re.sub(r"(\.v)|(\[.*?\])|(\(.*?\))", "", row["lu_name"]).strip()))],
         axis=1,
     )  # 注目する単語(動詞)の位置を取得
 
     tqdm.pandas(desc="target_word")
     df["target_word"] = df.progress_apply(
-        lambda x: x["preprocessed_text"].split()[x["target_word_idx"]], axis=1
+        lambda x: x["preprocessed_text"][x["target_word_idx"][0] : x["target_word_idx"][1]], axis=1
     )  # 注目する単語(動詞)を取得
 
-    df = df.drop_duplicates(subset=["preprocessed_text", "target_word_idx"])  # 重複を削除
+    df = df.drop_duplicates(subset=["preprocessed_text", "target_word"])  # 重複を削除
 
     preprocessed_exemplars: list[FramenetData] = [
         FramenetData(
@@ -161,23 +165,19 @@ def main():
             target_word=row["target_word"],
             target_word_idx=row["target_word_idx"],
             preprocessed_text=row["preprocessed_text"],
-            preprocessed_target_widx=row["preprocessed_target_widx"],
+            preprocessed_lu_idx=row["preprocessed_lu_idx"],
             frame_name=row["frame_name"],
             frame_id=row["frame_id"],
             lu_name=row["lu_name"],
             lu_id=row["lu_id"],
-            fe_widx=row["fe_widx"],
+            fe_idx=row["fe_idx"],
         )
         for _, row in df.iterrows()
     ]
 
-    df_sentences = df.drop_duplicates(subset=["preprocessed_text"])
-
-    docs = [nlp(text) for text in tqdm(df_sentences["preprocessed_text"], desc="Processing texts with NLP")]
-
     word_lists: list[FramenetWordList] = [
-        make_word_list(FramenetId(id=row["id_data"]["id"]), doc)
-        for doc, (_, row) in zip(docs, df_sentences.iterrows(), strict=True)
+        make_word_list(FramenetId(id=dictionary["id_data"]["id"]), dictionary["doc"])
+        for dictionary in df[["doc", "id_data"]].to_dict(orient="records")
     ]
 
     with open(args.output_exemplar_file, "w") as f:
